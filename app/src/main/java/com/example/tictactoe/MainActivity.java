bdf2334c59d50b66752ee22a6e83b5d45962e527
Alexis Sarlat <alexis.sarlat@ynov.com>
2020-02-18 17:32:04 +0100

    commit total images

--- app/src/main/java/com/example/tictactoe/MainActivity.java
+++ app/src/main/java/com/example/tictactoe/MainActivity.java
@@ -3,7 +3,10 @@ package com.example.tictactoe;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.ImageButton;
+import android.widget.Toast;
 
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
@@ -14,17 +17,32 @@ public class MainActivity extends AppCompatActivity {
     DatabaseReference boardRef = database.getReference("board");
 
     private static ImageButton IB1, IB2, IB3, IB4, IB5, IB6, IB7, IB8, IB9 ;
-
+    private boolean joueur1Joue = true;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        boolean joueur1Joue = true;
+
         setContentView(R.layout.activity_main);
-
-        IB1
-
+        OnClickButtonListener(savedInstanceState);
     }
 
-
-
+    public void OnClickButtonListener(Bundle savedInstanceState) {
+        IB9 = (ImageButton) findViewById(R.id.IB9);
+        IB9.setOnClickListener(new View.OnClickListener() {
+            @
+                    Override
+            public void onClick(View v) {
+                Toast toast = Toast.makeText(getApplicationContext(), "Bip ! Bip ! I'm a toast !", Toast.LENGTH_SHORT);
+                toast.show();
+                if (joueur1Joue){
+                    IB9.setBackgroundResource(R.drawable.un);
+                    joueur1Joue = false;
+                }
+                else {
+                    IB9.setImageResource(R.drawable.deux);
+                    joueur1Joue = true;
+                }
+            }
+        });
+    }
 }
