3e12e885662867c85107b10b4ad9cec23c3e6c50
Alexis Sarlat <alexis.sarlat@ynov.com>
2020-02-18 15:37:29 +0100

    commit test total files

--- app/src/main/java/com/example/tictactoe/MainActivity.java
+++ app/src/main/java/com/example/tictactoe/MainActivity.java
@@ -3,12 +3,20 @@ package com.example.tictactoe;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ImageButton;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static ImageButton IB1, IB2, IB3, IB4, IB5, IB6, IB7, IB8, IB9 ;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        boolean joueur1Joue = true;
         setContentView(R.layout.activity_main);
+
     }
+
+
+
 }
