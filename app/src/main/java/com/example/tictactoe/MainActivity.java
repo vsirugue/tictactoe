4941843d916fbc3c3b5d6960bbaba220d69e20da
Valentin_S <valentin.sirugue@ynov.com>
2020-02-18 15:45:31 +0100

    firebase mainactivity

--- app/src/main/java/com/example/tictactoe/MainActivity.java
+++ app/src/main/java/com/example/tictactoe/MainActivity.java
@@ -5,7 +5,13 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.ImageButton;
 
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
 public class MainActivity extends AppCompatActivity {
+
+    FirebaseDatabase database = FirebaseDatabase.getInstance();
+    DatabaseReference boardRef = database.getReference("board");
 
     private static ImageButton IB1, IB2, IB3, IB4, IB5, IB6, IB7, IB8, IB9 ;
 
@@ -14,6 +20,8 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         boolean joueur1Joue = true;
         setContentView(R.layout.activity_main);
+
+        IB1
 
     }
 
